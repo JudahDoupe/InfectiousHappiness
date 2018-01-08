60eb3d20db2ce6d4a5fbbd83f931690e0bf44ea6
Alec Ellsworth <alec.ells123@gmail.com>
2018-01-08 15:32:02 -0500

    I'm Alec and I am great at commitment.

---
+++ Assets/LevelData/LevelTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTemplate : LevelBuilder
+{
+
+    public override Level Build()
+    {
+        Setup();
+
+        BuildStartingRoom();
+
+        return Level;
+    }
+
+    void BuildStartingRoom()
+    {
+        CurrentRoom = 0;
+
+        PlaceFloor(new Vector3(0, 0, 0), new Vector3(47, 0, 47));
+        PlaceBlock(new Vector3(2, 0, 0), VoxelWorld.Instance.FloorBlock);
+    }
+
+}
